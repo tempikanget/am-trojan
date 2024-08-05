c7513c61c6a040ddc31473227223c4158949d208
tempikanget <124062317+tempikanget@users.noreply.github.com>
2024-08-05 08:51:45 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // _worker.src.js
 import { connect } from "cloudflare:sockets";
-let password = 'auto';
-let proxyIP = '';
+let password = 'trojan';
+let proxyIP = '13.228.155.72';
 // The user name and password do not contain special characters
 // Setting the address will ignore proxyIP
 // Example:  user:pass@host:port  or  host:port
